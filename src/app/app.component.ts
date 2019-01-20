ebdaddd6b3b1ce3b63492690b6e595936fa29f4e
CarlosValenca <carlos_valenca@uol.com.br>
2019-01-20 18:48:50 -0200

    21 - Angular Startup Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Template';
+  title = 'Tudo pronto para começar :)';
+  isCollapsed: boolean = true; // começa fechado
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CollapseModule.forRoot() // Para aplicar no módulo raiz
   ],
   providers: [],
   bootstrap: [AppComponent]
